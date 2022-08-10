90a566b83e3ca680eaf43743af876b66019c216b
silvicky <vickyisnotaboy@gmail.com>
2022-08-10 22:29:30 +0800

    Now lists all usable classes.

--- src/main/java/io/silvicky/Koshka/KoshkaManager.java
+++ src/main/java/io/silvicky/Koshka/KoshkaManager.java
@@ -4,13 +4,16 @@ import javax.swing.*;
 import javax.swing.GroupLayout.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.Constructor;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public class KoshkaManager extends JFrame {
-    JTextField jTextField;
+    JComboBox classList;
     JButton addButton,delButton;
     Queue<Koshka> queue;
     public static final String ver="0.2";
@@ -18,13 +21,13 @@ public class KoshkaManager extends JFrame {
     {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Koshka Manager");
-        jTextField=new JTextField("io.silvicky.Koshka.Koshka",10);
+        classList=new JComboBox<>(findAllClasses("io.silvicky.Koshka").toArray(new Class[0]));
         addButton=new JButton("+");
         delButton=new JButton("-");
         addButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 try {
-                    Class<? extends io.silvicky.Koshka.Koshka> requiredClass=Class.forName(jTextField.getText()).asSubclass(io.silvicky.Koshka.Koshka.class);
+                    Class requiredClass= (Class) classList.getSelectedItem();
                     Constructor<? extends io.silvicky.Koshka.Koshka> constructor=requiredClass.getConstructor();
                     Koshka object=constructor.newInstance();
                     queue.add(object);
@@ -49,7 +52,7 @@ public class KoshkaManager extends JFrame {
         h1.addContainerGap();
         h1.addComponent(addButton);
         h1.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED);
-        h1.addComponent(jTextField, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE);
+        h1.addComponent(classList, GroupLayout.DEFAULT_SIZE, 321, Short.MAX_VALUE);
         h1.addPreferredGap(LayoutStyle.ComponentPlacement.RELATED);
         h1.addComponent(delButton);
         h1.addContainerGap();
@@ -60,7 +63,7 @@ public class KoshkaManager extends JFrame {
         v1.addContainerGap();
         ParallelGroup v2 = layout.createParallelGroup(GroupLayout.Alignment.BASELINE);
         v2.addComponent(addButton);
-        v2.addComponent(jTextField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE);
+        v2.addComponent(classList, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE);
         v2.addComponent(delButton);
         v1.addGroup(v2);
         v1.addContainerGap();
@@ -68,6 +71,26 @@ public class KoshkaManager extends JFrame {
         layout.setVerticalGroup(vGroup);
         pack();
         setVisible(true);
+    }
+    public List<Class> findAllClasses(String packageName) {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> getClass(line, packageName))
+                .filter(Objects::nonNull)
+                .filter(Koshka.class::isAssignableFrom)
+                .collect(Collectors.toList());
+    }
+    private static Class getClass(String className, String packageName) {
+        try {
+            return Class.forName(packageName + "."
+                    + className.substring(0, className.lastIndexOf('.')));
+        } catch (ClassNotFoundException ignored) {
+
+        }
+        return null;
     }
     public static void main(String[] args)
     {
